db263f8beac5ea8ffbd7e10837cd72df5f8f88cd
Matt Karl <matt@cloudkid.com>
2014-09-07 14:56:24 -0400

    Auto-generated commit

---
+++ api.js
@@ -0,0 +1,24 @@
+YUI.add("yuidoc-meta", function(Y) {
+   Y.YUIDoc = { meta: {
+    "classes": [
+        "Array.prototype",
+        "Emitter",
+        "Particle",
+        "ParticleUtils"
+    ],
+    "modules": [
+        "cloudkid",
+        "global"
+    ],
+    "allModules": [
+        {
+            "displayName": "cloudkid",
+            "name": "cloudkid"
+        },
+        {
+            "displayName": "global",
+            "name": "global"
+        }
+    ]
+} };
+});
