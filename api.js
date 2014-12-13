ed19daf5952db5b4eb20195ce34de45234d851a5
andrewstart <andrewstart@cloudkid.com>
2014-12-13 18:32:22 -0500

    Auto-generated commit

--- api.js
+++ api.js
@@ -1,10 +1,12 @@
 YUI.add("yuidoc-meta", function(Y) {
    Y.YUIDoc = { meta: {
     "classes": [
+        "AnimatedParticle",
         "Array.prototype",
         "Emitter",
         "Particle",
-        "ParticleUtils"
+        "ParticleUtils",
+        "PathParticle"
     ],
     "modules": [
         "cloudkid",
