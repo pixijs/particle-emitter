5876bd818da750ef1f000adcd3e2a61255ac181d
Dave Moore <themoonrat@gmail.com>
2017-01-13 10:35:00 +0000

    Updated builds

--- dist/pixi-particles.js
+++ dist/pixi-particles.js
@@ -1503,8 +1503,8 @@ if(!Array.prototype.random)
 	});
 
 	/**
-	 * If particles should be emitted during update() calls. Setting this to false
-	 * stops new particles from being created, but allows existing ones to die out.
+	 * If the update function is called automatically from the shared ticker.
+	 * Setting this to false requires calling the update function manually.
 	 * @property {Boolean} emit
 	 */
 	Object.defineProperty(p, "autoUpdate",
@@ -1512,11 +1512,11 @@ if(!Array.prototype.random)
 		get: function() { return this._autoUpdate; },
 		set: function(value)
 		{
-			if (this._autoUpdate && value === false)
+			if (this._autoUpdate && !value)
 			{
 				ticker.remove(this.update, this);
 			}
-			else if (!this._autoUpdate && value === true)
+			else if (!this._autoUpdate && value)
 			{
 				ticker.add(this.update, this);
 			}
@@ -1533,7 +1533,7 @@ if(!Array.prototype.random)
 	{
 		if (this._autoUpdate)
 		{
-			delta *= 0.01;
+			delta = delta / ticker.speed / PIXI.settings.TARGET_FPMS / 1000;
 		}
 
 		//if we don't have a parent to add particles to, then don't do anything.
