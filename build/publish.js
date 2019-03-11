53007b5a31b4b9378ecbbb0da859ad2e37647813
andrewstart <start.andrew@gmail.com>
2019-03-11 11:34:26 -0400

    Tweak script for modifying package.json around publishing.

--- build/publish.js
+++ build/publish.js
@@ -5,14 +5,14 @@ const exec = require('child_process').exec;
 
 // remove private flag that yarn requires for workspace root
 pkg.private = false;
-fs.writeFileSync(path.resolve('./package.json'), JSON.stringify(pkg, null, 2));
+fs.writeFileSync(path.resolve('./package.json'), JSON.stringify(pkg, null, 2) + '\n');
 exec('npm publish', {
 	env: process.env,
 	cwd: process.cwd()
 }, function(err, stdOut, stdErr) {
 	// restore private flag
 	pkg.private = true;
-	fs.writeFileSync(path.resolve('./package.json'), JSON.stringify(pkg, null, 2));
+	fs.writeFileSync(path.resolve('./package.json'), JSON.stringify(pkg, null, 2) + '\n');
 	if (err)
 	{
 		console.error(stdErr);
