bce5e4a2972e37e64e1e7bbcf8f2d72600b02704
andrewstart <start.andrew@gmail.com>
2020-09-18 09:40:18 -0400

    Some documentation for the LinkedListContainer.

--- src/LinkedListContainer.ts
+++ src/LinkedListContainer.ts
@@ -1,11 +1,19 @@
 import { Container, DisplayObject, Renderer, Rectangle, MaskData } from 'pixi.js';
 
+/** Interface for a child of a LinkedListContainer (has the prev/next properties added) */
 export interface LinkedListChild extends DisplayObject
 {
     nextChild: LinkedListChild|null;
     prevChild: LinkedListChild|null;
 }
 
+/**
+ * A semi-experimental Container that uses a doubly linked list to manage children instead of an
+ * array. This means that adding/removing children often is not the same performance hit that
+ * it would to be continually pushing/splicing.
+ * However, this is primarily intended to be used for heavy particle usage, and may not handle
+ * edge cases well if used as a complete Container replacement.
+ */
 export class LinkedListContainer extends Container
 {
     private _firstChild: LinkedListChild|null = null;
