deaba535155d50f5bd0890166b63c4225081e783
Andrew Start <start.andrew@gmail.com>
2021-08-22 21:08:05 -0400

    Adjust publish script to handle the new scope

--- build/publish.js
+++ build/publish.js
@@ -6,7 +6,7 @@ const exec = require('child_process').exec;
 // remove private flag that yarn requires for workspace root
 pkg.private = false;
 fs.writeFileSync(path.resolve('./package.json'), JSON.stringify(pkg, null, 2) + '\n');
-exec('npm publish', {
+exec('npm publish --access public', {
 	env: process.env,
 	cwd: process.cwd()
 }, function(err, stdOut, stdErr) {
