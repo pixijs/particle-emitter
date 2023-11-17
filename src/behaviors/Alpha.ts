996da2a60dd2cc764bedb6d8bec3ac39fd8ca167
Sijia Wang <30830531+asd4486@users.noreply.github.com>
2023-11-17 19:46:08 +0100

    improve behaviors

--- src/behaviors/Alpha.ts
+++ src/behaviors/Alpha.ts
@@ -10,10 +10,10 @@ import { BehaviorEditorConfig } from './editor/Types';
  * Example config:
  * ```javascript
  * {
- *     type: 'alpha',
- *     config: {
- *         alpha: {
- *              list: [{value: 0, time: 0}, {value: 1, time: 0.25}, {value: 0, time: 1}]
+ *     "type": "alpha",
+ *     "config": {
+ *         "alpha": {
+ *              "list": [{"value": 0, "time": 0}, {"value": 1, "time": 0.25}, {"value": 0, "time": 1}]
  *         },
  *     }
  * }
@@ -21,37 +21,38 @@ import { BehaviorEditorConfig } from './editor/Types';
  */
 export class AlphaBehavior implements IEmitterBehavior
 {
-    public static type = 'alpha';
-    public static editorConfig: BehaviorEditorConfig = null;
+	public static type = 'alpha';
+	public static editorConfig: BehaviorEditorConfig = null;
 
-    public order = BehaviorOrder.Normal;
-    private list: PropertyList<number>;
-    constructor(config: {
-        /**
-         * Transparency of the particles from 0 (transparent) to 1 (opaque)
-         */
-        alpha: ValueList<number>;
-    })
-    {
-        this.list = new PropertyList(false);
-        this.list.reset(PropertyNode.createList(config.alpha));
-    }
+	public order = BehaviorOrder.Normal;
+	private list: PropertyList<number>;
+	constructor(config: {
+		/**
+		 * Transparency of the particles from 0 (transparent) to 1 (opaque)
+		 */
+		alpha: ValueList<number>;
+	})
+	{
+	    this.list = new PropertyList(false);
+	    this.list.reset(PropertyNode.createList(config.alpha));
+	}
 
-    initParticles(first: Particle): void
-    {
-        let next = first;
+	initParticles(first: Particle): void
+	{
+	    let next = first;
 
-        while (next)
-        {
-            next.alpha = this.list.first.value;
-            next = next.next;
-        }
-    }
+	    while (next)
+	    {
+	        next.config.originAlpha = next.config.originAlpha ?? 1;
+	        next.alpha = next.config.originAlpha * this.list.first.value;
+	        next = next.next;
+	    }
+	}
 
-    updateParticle(particle: Particle): void
-    {
-        particle.alpha = this.list.interpolate(particle.agePercent);
-    }
+	updateParticle(particle: Particle): void
+	{
+	    particle.alpha = this.list.interpolate(particle.agePercent);
+	}
 }
 
 /**
@@ -60,38 +61,56 @@ export class AlphaBehavior implements IEmitterBehavior
  * Example config:
  * ```javascript
  * {
- *     type: 'alphaStatic',
- *     config: {
- *         alpha: 0.75,
+ *     "type": "alphaStatic",
+ *     "config": {
+ *         "alpha": 0.75,
  *     }
  * }
  * ```
+ * or
+ * ```javascript
+ * {
+ *     "type": "alphaStatic",
+ *     "config": {
+ *         "alpha": {"min": 0.5, "max": 1}
+ *     }
+ * }
  */
+
+type AlphaData = {
+	min: number;
+	max: number;
+};
 export class StaticAlphaBehavior implements IEmitterBehavior
 {
-    public static type = 'alphaStatic';
-    public static editorConfig: BehaviorEditorConfig = null;
+	public static type = 'alphaStatic';
+	public static editorConfig: BehaviorEditorConfig = null;
 
-    public order = BehaviorOrder.Normal;
-    private value: number;
-    constructor(config: {
-        /**
-         * Transparency of the particles from 0 (transparent) to 1 (opaque)
-         */
-        alpha: number;
-    })
-    {
-        this.value = config.alpha;
-    }
+	public order = BehaviorOrder.Normal;
+	private value: number | AlphaData;
+	constructor(config: {
+		/**
+		 * Transparency of the particles from 0 (transparent) to 1 (opaque)
+		 */
+		alpha: number | AlphaData;
+	})
+	{
+	    this.value = config.alpha;
+	}
 
-    initParticles(first: Particle): void
-    {
-        let next = first;
+	initParticles(first: Particle): void
+	{
+	    let next = first;
 
-        while (next)
-        {
-            next.alpha = this.value;
-            next = next.next;
-        }
-    }
+	    while (next)
+	    {
+	        const val
+				= typeof this.value === 'number'
+				    ? this.value
+				    : (Math.random() * (this.value.max - this.value.min)) + this.value.min;
+
+	        next.config.originAlpha = next.alpha = Math.min(val, 1);
+	        next = next.next;
+	    }
+	}
 }
